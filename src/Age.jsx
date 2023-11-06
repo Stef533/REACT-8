4e8309c8753517a5899fbe7f2967664427deb51b
Stefan Petrea <137123241+Stef533@users.noreply.github.com>
2023-11-06 15:55:12 +0100

    Add files via upload

---
+++ src/Age.jsx
@@ -0,0 +1,5 @@
+const Age = () => {
+  return <p>Your age is </p>;
+};
+
+export default Age;
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Welcome from './Welcome';
+
+function App() {
+  return (
+    <div>
+      <h1>Ciao Mondo!</h1>
+      <Welcome name="John" age={22} />
+    </div>
+  );
+}
+
+export default App;
